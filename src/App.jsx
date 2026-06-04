599a099c73d79e05f43c68ca14677ad36efc0fc8
HamayaYuuma <241113@st.yoshida-g.ac.jp>
2026-06-04 10:58:42 +0900

    props練習

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import ApiSample from './ApiSample'
+import Greeting from './Greeting'
 
 function App() {
   const [todos, setTodos] = useState(() => { //ローカルストレージにtodosがあればそれを返し、なければ空の配列を返す
@@ -56,32 +57,10 @@ function App() {
 
 
   return (
-  <div>
-    <h1>TODOアプリ</h1>
-    <div>
-      <button onClick={() => setActiveTab('local')} style={{ fontWeight: activeTab === 'local' ? 'bold' : 'normal' }}>自分のTODO</button>
-      <button onClick={() => setActiveTab('api')} style={{ fontWeight: activeTab === 'api' ? 'bold' : 'normal' }}>サンプル（API）</button>
-    </div>
-    {activeTab === 'local' && (
-      <>
-        <button onClick={() => setOrder(order === 'asc' ? 'desc' : 'asc')}>並び替え ({order === 'asc' ? '古い順' : '新しい順'})</button>
-        <TodoForm onAdd={addTodo} />
-        <button onClick={clearAll} style={{margin: '8px 0'}}>
-          全削除
-        </button>
-        <p>全{todos.length}件/完了{todos.filter((todo) => todo.completed).length}件/未完了{todos.filter((todo) => !todo.completed).length}件</p>
-          
-        <TodoList todos={sortedTodos} onDelete={deleteTodo} toggleTodo={toggleTodo} />
-      </>
-    )}
-    {activeTab === 'api' && (
-      <>
-        <h2>API取得サンプル</h2>
-        <button onClick={() => setReloadKey((prev) => prev + 1)}>再読み込みボタン</button>
-        <ApiSample reloadKey={reloadKey} />
-      </>
-    )}
-  </div>
+  <>
+    <h1>見出しを書く</h1>
+    <Greeting name="cyber" greet="こんばんは"/>
+  </>
  )
 }
 
